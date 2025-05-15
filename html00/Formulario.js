b4be584559ec762b5c0128844e33e64e148e0129
JAMtheProgrammer <andradejohan651>
2025-05-14 18:38:01 -0600

    refactorización

--- html00/Formulario.js
+++ html00/Formulario.js
@@ -1,15 +1,38 @@
 console.log("Inicio");
 
-const boton = document.getElementById("Enviar")
+//function mostrar(){
+//    console.log("los parametros")
+//        let user = document.getElementById("user")
+//        let password = document.getElementById("password")
+//        console.log(password.value)        
+//        console.log(user.value)
+//}
 
-console.log(boton);
-
-boton.addEventListener("click", 
-    () => {
-        console.log("los parametros")
+const mostrar = () => {
+    console.log("los parametros")
         let user = document.getElementById("user")
         let password = document.getElementById("password")
+        console.log(password.value)        
         console.log(user.value)
-        console.log(password.value)
-    }
-)
+
+        let formulario = document.getElementsByTagName("form")
+        console.log(formulario)
+        user.setAttribute("name", "q")
+        password.setAttribute("name", "password")
+        formulario[0].setAttribute("action","https://google.com/search")
+        formulario[0].submit()
+}
+
+const boton = document.getElementById("enviar")
+boton.addEventListener("click", mostrar)
+//boton.addEventListener("click", 
+//    () => {
+//        console.log("los parametros")
+//        let user = document.getElementById("user")
+//        let password = document.getElementById("password")
+//        console.log(password.value)        
+//        console.log(user.value)
+//
+//
+//    }
+//)
---
+++ html00/tarea/tarea.md
@@ -0,0 +1,19 @@
+# Ejercicio de factorización
+## Objetivo
+Refactorizar todas la páginas HTML que tengan asociado o contengan javascrip. La refactorización consiste en pasar todo el código a su forma de módulos.
+
+## Lista de páginas
+* Página 1
+* Página 2
+* Página 5
+* Página 6
+* Página 7
+
+## Reporte análitico
+Van a comparar y explicar las diferencias de ejecusión entre la versión no modular y la versión modular. Ejemplo de ello, sería explicar los tiempos de carga adjuntando capturas de pantalla donde se vean ambos casos.
+
+```
+git add tareas/
+git commit -m "refactorización"
+git push
+```
